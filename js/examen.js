28666c0344f9ef8422021d50121cdfd2c8c905c7
MarlonMazariego <61699445+MarlonMazariego@users.noreply.github.com>
2022-01-04 20:36:55 -0600

    Add files via upload

---
+++ js/examen.js
@@ -0,0 +1,14 @@
+function enviar(){
+     alertify.success('Success message');
+ 
+}
+
+function inicio(){
+     alertify.success('Sesion iniciada');
+ 
+}
+  
+function hola(){
+    alert('Bienvenido');
+}
+
